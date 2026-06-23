3e1d8ee2686264c8c776249fe139bf3169898330
Sabri Pucheta <sabrinaapucheta@gmail.com>
2026-06-22 21:07:24 -0300

    se realizo el ejercicio 3

---
+++ js/ejercicio3.js
@@ -0,0 +1,8 @@
+
+let texto_ingresado = "";
+let cadena_texto_ingresado = "";
+do {
+    let texto_ingresado = prompt("Ingrese un texto: ")
+    cadena_texto_ingresado = cadena_texto_ingresado + texto_ingresado
+} while (confirm("Desea ingresar otro texto?"))
+    document.write(`El texto ingresado es: ${cadena_texto_ingresado}`);
