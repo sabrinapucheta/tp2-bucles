e27fc46a503a5e7c0519664491aadb107b113bb7
Sabri Pucheta <sabrinaapucheta@gmail.com>
2026-06-25 21:55:06 -0300

    se corrigio el ejercicio 9, se arreglo el orden de la variable num para que las etiquetas p envueltan los numeros

--- js/ejercicio9.js
+++ js/ejercicio9.js
@@ -5,14 +5,14 @@ for (let i= 1 ; i<= 500 ; i++) {
 
 
  if ( (i % 4) === 0) {
-    numeros = '<p>' + numeros + i + ' (Multiplo de 4) </p>'
+    numeros = numeros + '<p>' + i + ' (Multiplo de 4) </p>'
  } else if ((i % 5) === 0){
 
-    numeros = '<p>' + numeros + i + '</p> <p> ------------------ </p>'
+    numeros = numeros + '<p>' + i + '</p> <p> ------------------ </p>'
 
 
  } else {
-    numeros = '<p>' + numeros + i + '</p>'
+    numeros = numeros + '<p>' + i + '</p>'
  }
 
 }
